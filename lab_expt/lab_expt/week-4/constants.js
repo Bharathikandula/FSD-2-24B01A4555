9ec213435417d5bbdb2480add8e9cec3cde90070
Kandula Bharathi <bharathi42440@gmail.com>
2026-08-01 00:12:51 +0530

    Remove all JS files

--- lab_expt/lab_expt/week-4/constants.js
+++
@@ -1,2 +0,0 @@
-export const gst_rate = 0.02; //2% gst on bus ticket
-export const ticket_fee = 45; //bus ticket fees
--- lab_expt/lab_expt/week-4/main.js
+++
@@ -1,8 +0,0 @@
-import { Ticket } from "./ticket.js";
-const traveler = {
-    name: "lahari",
-    age: 20,
-    seatpreference: 'middle'
-};
-const myticket = new Ticket(traveler, 1500, 156367);
-myticket.print();
